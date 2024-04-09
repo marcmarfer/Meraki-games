2390e4c66e8a1e812bca942825eb706c0f33f0e7
Sergi Farga Expósito <sergifarexp@campus.monlau.com>
2024-04-09 19:18:30 +0200

    feature/add_menu

---
+++ NeuroCrib-games/src/Menu.jsx
@@ -0,0 +1,19 @@
+import { Link } from "react-router-dom";
+
+export default function Menu() {
+
+  return (
+    <>
+      <h1 className='text-blue-700 text-5xl text-center pt-40'>¡Bienvenido a NeuroLab!</h1>
+      <h3 className="text-blue-600 text-xl text-center pt-10">Por favor, selecciona a qué quieres jugar:</h3>
+      <ul className="flex flex-wrap justify-center text-blue-400 text-lg text-center pt-10">
+        <li className="w-1/2 p-5">
+          <Link to="/patitos">Patitos</Link>
+        </li>
+        <li className="w-1/2 p-5">Simón dice</li>
+        <li className="w-1/2 p-5">Test</li>
+        <li className="w-1/2 p-5">Test</li>
+      </ul>
+    </>
+  )
+}
---
+++ NeuroCrib-games/src/juegos/patitos/Patitos.jsx
@@ -0,0 +1,9 @@
+import React, { useEffect, useState } from 'react';
+
+
+export default function Patitos() {
+  
+  return (
+    <h1>HOLA BUENAS</h1>
+  );
+}

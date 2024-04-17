6c3507667c40df6fa5d9c5328147d53032fa68b4
Marc Marqués Fernández <marcmf2004@gmail.com>
2024-04-17 12:48:09 +0200

    Now images are permanently being rendered no matter what by passing the context as the target of the image rendering. Also repositioned all the objects in the canvas

--- src/juegos/kinematics/Kinematics.jsx
+++ src/juegos/kinematics/Kinematics.jsx
@@ -43,34 +43,55 @@ const Kinematics = () => {
 
     useEffect(() => {
         const newCanvas = document.createElement('canvas');
+        const canvasWidth = Math.floor(window.innerWidth * 0.7);
+        const canvasHeight = Math.floor(window.innerHeight * 0.7);
+        newCanvas.width = canvasWidth;
+        newCanvas.height = canvasHeight;
         setCanvas(newCanvas);
-        const newContext = newCanvas.getContext('2d');
+    }, []);
+    
+    useEffect(() => {
+        if (!canvas) return;
+    
+        const newContext = canvas.getContext('2d');
         setContext(newContext);
-
+        
+        //set images for each sprite and set their initial position on the canvas
         const backgroundImage = new Image();
         backgroundImage.src = 'src/juegos/kinematics/sprites/Utils/GameBackground.png';
         setBackgroundImage(backgroundImage);
-
+    
         const helicopterImage = new Image();
         helicopterImage.src = 'src/juegos/kinematics/sprites/Vehicles/Helicopter.png';
-        setHelicopter(prevState => ({ ...prevState, image: helicopterImage }));
-
+        setHelicopter(prevState => ({
+            ...prevState,
+            x: (canvas.width - 200) - (helicopter.width / 2),
+            y: (canvas.height / 2 - 150) - (helicopter.width  / 2),
+            image: helicopterImage
+        }));
+    
         const planeImage = new Image();
         planeImage.src = 'src/juegos/kinematics/sprites/Vehicles/Plane.png';
-        setPlane(prevState => ({ ...prevState, image: planeImage }));
-
+        setPlane(prevState => ({
+            ...prevState,
+            x: 200 - (plane.width / 2),
+            y: 200,
+            image: planeImage
+        }));
+    
         const carImage = new Image();
         carImage.src = 'src/juegos/kinematics/sprites/Vehicles/Car.png';
-        setCar(prevState => ({ ...prevState, image: carImage }));
-    }, []);
-
+        setCar(prevState => ({
+            ...prevState,
+            x: 200 - (car.width / 2),
+            y: canvas.height - 200,
+            image: carImage
+        }));
+    }, [canvas]);
+    
     useEffect(() => {
         if (canvas && context && !canvasContainerAdded) {
-            const canvasWidth = Math.floor(window.innerWidth * 0.7);
-            const canvasHeight = Math.floor(window.innerHeight * 0.7);
-            canvas.width = canvasWidth;
-            canvas.height = canvasHeight;
-
+            console.log(canvas.width, canvas.height)
             let requestId;
             let lastTimestamp = 0;
             let time = 0;
@@ -110,25 +131,29 @@ const Kinematics = () => {
                 }));
             }
 
-            let drawImage = (e, image) => {
+            let renderImage = (e, image) => {
                 if (image === backgroundImage) {
                     e.target.context.drawImage(image, 0, 0, canvas.width, canvas.height);
                 } else if (image === helicopter.image) {
-                    e.target.context.drawImage(image, (canvas.width - 200) - (helicopter.width / 2), (canvas.height / 2 - 150) - (helicopter.height / 2), helicopter.width, helicopter.height);
+                    e.target.context.drawImage(image, helicopter.x, helicopter.y, helicopter.width, helicopter.height);
                 } else if (image === plane.image) {
-                    e.target.context.drawImage(image, 200 - (plane.width / 2), 200, plane.width, plane.height);
+                    e.target.context.drawImage(image, plane.x, plane.y, plane.width, plane.height);
                 } else if (image === car.image) {
-                    e.target.context.drawImage(image, 200 - (car.width / 2), canvas.height - 200, car.width, car.height);
+                    e.target.context.drawImage(image, car.x, car.y, car.width, car.height);
                 }
             }
-
+            
             let draw = () => {
+                context.drawImage(backgroundImage, 0, 0, canvas.width, canvas.height);
                 const imagesArray = [backgroundImage, helicopter.image, plane.image, car.image];
+            
+                const animate = () => {
+                    imagesArray.forEach((image) => {
+                        renderImage({ target: { context } }, image);
+                    });
+                };
 
-                imagesArray.forEach((image) => {
-                    image.context = context;
-                    image.onload = (e) => drawImage(e, image);
-                });
+                animate();
             }
 
             function render() {
@@ -137,6 +162,7 @@ const Kinematics = () => {
 
             window.addEventListener("resize", fitCanvasToScreen);
 
+            //function to resize the canvas in case the window is resized
             function fitCanvasToScreen() {
                 canvas.width = Math.floor(window.innerWidth * 0.7);
                 canvas.height = Math.floor(window.innerHeight * 0.7);

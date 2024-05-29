300c0b8d23eef9d951f8d1b8f7da66d6a904dfd9
unknown <marcmf2004@gmail.com>
2024-05-29 19:13:13 +0200

    sending all data needed to server in Words Box 1

--- src/juegos/words-box-1/words-box-1.jsx
+++ src/juegos/words-box-1/words-box-1.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import './words-box-1.css';
+import { useLocation } from 'react-router-dom';
 
 const WordsBox1 = () => {
   const [words, setWords] = useState([
@@ -15,9 +15,17 @@ const WordsBox1 = () => {
     { id: '10', content: 'zapato' }
   ]);
   const [wordsWithA, setWordsWithA] = useState([]);
-  const [selectedLevel, setSelectedLevel] = useState("easy"); // Nivel por defecto
+  const [selectedLevel, setSelectedLevel] = useState("easy");
   const [score, setScore] = useState(0);
   const [mistakes, setMistakes] = useState(0);
+
+  const location = useLocation();
+  const searchParams = new URLSearchParams(location.search);
+
+  const studentId = searchParams.get('id');
+  const testId = searchParams.get('test_id');
+  const gameId = searchParams.get('game_id');
+  const gameTestId = searchParams.get('gameTest_id');
 
   useEffect(() => {
     let newWords = [];
@@ -88,13 +96,13 @@ const WordsBox1 = () => {
       setWords(prevState => prevState.filter(word => word.id !== id));
 
       if (selectedLevel === "easy" && content.startsWith('a')) {
-        setScore(score + 1); // Incrementar el puntaje si la palabra empieza con 'a'
+        setScore(score + 1);
       } else if (selectedLevel === "medium" && content.startsWith('a') && content.endsWith('s')) {
-        setScore(score + 1); // Incrementar el puntaje si la palabra empieza con 'a' y termina con 's'
+        setScore(score + 1);
       } else if (selectedLevel === "hard" && content.startsWith('c') && content.includes('a')) {
-        setScore(score + 1); // Incrementar el puntaje si la palabra empieza con 'c' y contiene 'a'
+        setScore(score + 1);
       } else {
-        setMistakes(mistakes + 1); // Incrementar los errores si no cumple con las condiciones
+        setMistakes(mistakes + 1);
       }
     }
   };
@@ -105,11 +113,14 @@ const WordsBox1 = () => {
 
   const enviarDatosAlServidor = () => {
     const data = {
-      student_id: 10502,
-      test_id: 2,
-      game_id: 2,
-      score: score,
-      errors: mistakes
+      student_id: parseInt(studentId),
+      test_id: parseInt(testId),
+      game_id: parseInt(gameId),
+      gameTest_id: parseInt(gameTestId),
+      time: 0,
+      score: parseInt(score),
+      errors: parseInt(mistakes),
+      played: "true"
     };
 
     fetch('https://neurolab-dev.alumnes-monlau.com/api/games', {
@@ -119,18 +130,18 @@ const WordsBox1 = () => {
       },
       body: JSON.stringify(data)
     })
-    .then(response => {
-      if (!response.ok) {
-        throw new Error('Error al enviar los datos al servidor');
-      }
-      return response.json();
-    })
-    .then(data => {
-      console.log('Datos enviados correctamente:', data);
-    })
-    .catch(error => {
-      console.error('Error:', error);
-    });
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Error al enviar los datos al servidor');
+        }
+        return response.json();
+      })
+      .then(data => {
+        console.log('Datos enviados correctamente:', data);
+      })
+      .catch(error => {
+        console.error('Error:', error);
+      });
   };
 
   const endGame = () => {
@@ -148,51 +159,53 @@ const WordsBox1 = () => {
   };
 
   return (
-    <div className='words-box-1-container'>
-      <div className="mb-4">
-        <h2>{renderTitle()}</h2>
-      </div>
-      <div className="mb-4">
-        <button onClick={() => setSelectedLevel("easy")} className={`mr-2 ${selectedLevel === "easy" ? "bg-yellow-300" : "bg-gray-300"} hover:bg-yellow-300 text-white font-bold py-2 px-4 rounded`}>Fácil</button>
-        <button onClick={() => setSelectedLevel("medium")} className={`mr-2 ${selectedLevel === "medium" ? "bg-yellow-300" : "bg-gray-300"} hover:bg-yellow-300 text-white font-bold py-2 px-4 rounded`}>Medio</button>
-        <button onClick={() => setSelectedLevel("hard")} className={`mr-2 ${selectedLevel === "hard" ? "bg-yellow-300" : "bg-gray-300"} hover:bg-yellow-300 text-white font-bold py-2 px-4 rounded`}>Hard</button>
-      </div>
-      <div
-        onDrop={(e) => onDrop(e, 'box-1')}
-        onDragOver={onDragOver}
-        style={{ border: '1px solid black', padding: '10px', width: '200px', minHeight: '100px' }}
-      >
-        {words.map(word => (
-          <div
-            key={word.id}
-            draggable
-            onDragStart={(e) => onDragStart(e, word.id, word.content)}
-            style={{ margin: '8px 0', cursor: 'move' }}
-          >
-            {word.content}
-          </div>
-        ))}
-      </div>
-      <div
-        onDrop={(e) => onDrop(e, 'box-a')}
-        onDragOver={onDragOver}
-        style={{ marginTop: '20px', border: '1px solid green', padding: '10px', width: '200px', minHeight: '100px' }}
-      >
-        {wordsWithA.map(word => (
-          <div key={word.id} style={{ margin: '8px 0' }}>
-            {word.content}
-          </div>
-        ))}
-      </div>
-      <div className="mt-4">
-        <p>Puntaje: {score}</p>
-        <p>Errores: {mistakes}</p>
-      </div>
-      <div className="mt-4">
-        <button onClick={endGame} className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">Terminar Juego</button>
+    <div className="flex flex-col items-center justify-center h-screen">
+      <div className='words-box-1-container'>
+        <div className="mb-4">
+          <h2>{renderTitle()}</h2>
+        </div>
+        <div className="mb-4">
+          <button onClick={() => setSelectedLevel("easy")} className={`mr-2 ${selectedLevel === "easy" ? "bg-yellow-300" : "bg-gray-300"} hover:bg-yellow-300 text-white font-bold py-2 px-4 rounded`}>Fácil</button>
+          <button onClick={() => setSelectedLevel("medium")} className={`mr-2 ${selectedLevel === "medium" ? "bg-yellow-300" : "bg-gray-300"} hover:bg-yellow-300 text-white font-bold py-2 px-4 rounded`}>Medio</button>
+          <button onClick={() => setSelectedLevel("hard")} className={`mr-2 ${selectedLevel === "hard" ? "bg-yellow-300" : "bg-gray-300"} hover:bg-yellow-300 text-white font-bold py-2 px-4 rounded`}>Hard</button>
+        </div>
+        <div
+          onDrop={(e) => onDrop(e, 'box-1')}
+          onDragOver={onDragOver}
+          style={{ border: '1px solid black', padding: '10px', width: '200px', minHeight: '100px' }}
+        >
+          {words.map(word => (
+            <div
+              key={word.id}
+              draggable
+              onDragStart={(e) => onDragStart(e, word.id, word.content)}
+              style={{ margin: '8px 0', cursor: 'move' }}
+            >
+              {word.content}
+            </div>
+          ))}
+        </div>
+        <div
+          onDrop={(e) => onDrop(e, 'box-a')}
+          onDragOver={onDragOver}
+          style={{ marginTop: '20px', border: '1px solid green', padding: '10px', width: '200px', minHeight: '100px' }}
+        >
+          {wordsWithA.map(word => (
+            <div key={word.id} style={{ margin: '8px 0' }}>
+              {word.content}
+            </div>
+          ))}
+        </div>
+        <div className="mt-4">
+          <p>Puntaje: {score}</p>
+          <p>Errores: {mistakes}</p>
+        </div>
+        <div className="mt-4">
+          <button onClick={endGame} className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">Terminar Juego</button>
+        </div>
       </div>
     </div>
   );
-};
+}
 
 export default WordsBox1;
